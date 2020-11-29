519e8d3826e90c1869a39e40878c9544c46496e1
malcolmkz <73642102+malcolmkz@users.noreply.github.com>
2020-11-29 16:22:24 +0100

    Develop (#2)
    * Committing change to config files
    
    * Create test
    
    * removing test file
    
    * changing gitignore
    
    * commit
    
    * commit
    
    Co-authored-by: Malek KAZDZAGHLI <malek.kazdaghli@sofrecom.com>

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DATABASE,
-    "host": process.env.POSTGRESS_HOST,
+    //"username": process.env.POSTGRESS_USERNAME,
+    //"password": process.env.POSTGRESS_PASSWORD,
+    //"database": process.env.POSTGRESS_DATABASE,
+    //"host": process.env.POSTGRESS_HOST,
+    "username": "udagramdev",
+    "password": "udagramdev",
+    "database": "udagramdev",
+    "host": "udagramdev.cwrygmngmbck.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
